7d6692ae30ee7c168f6e7ec3e44744f63c606930
Avsecz <zigaavsec@gmail.com>
2017-03-13 18:21:10 +0100

    Bump version: 0.4.5 → 0.4.6

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requirements = [
 
 setup(
     name='concise',
-    version='0.4.5',
+    version='0.4.6',
     description="CONCISE (COnvolutional Neural for CIS-regulatory Elements) is a model for predicting PTR features like mRNA half-life from cis-regulatory elements using deep learning. ",
     long_description=readme + '\n\n' + history,
     author="Žiga Avsec",
