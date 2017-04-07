2a7e1654e257df44987d8b77baea28365ecaa1c4
Avsecz <zigaavsec@gmail.com>
2017-04-07 14:09:00 +0200

    fix: run encodeRNAstructure from any directory
    bug: encodeRNAStructure_parallel not working

--- concise/preprocessing/structure.py
+++ concise/preprocessing/structure.py
@@ -1,9 +1,16 @@
 import numpy as np
 from concise.utils.fasta import write_fasta, iter_fasta
 from concise.preprocessing.sequence import pad_sequences
+from subprocess import call
 import os
+from pkg_resources import resource_filename
 
-RNAplfold_BIN_DIR = "concise/resources/RNAplfold"
+from joblib import Parallel, delayed
+from multiprocessing import Pool
+import multiprocessing
+
+
+RNAplfold_BIN_DIR = os.path.dirname(resource_filename('concise', 'resources/RNAplfold/E_RNAplfold'))
 RNAplfold_PROFILES_EXECUTE = ["H", "I", "M", "E"]
 RNAplfold_PROFILES = ["Pairedness", "Hairpin loop", "Internal loop", "Multi loop", "External region"]
 
@@ -16,16 +23,19 @@ def run_RNAplfold(input_fasta, tmpdir, W=240, L=160, U=1):
        U, Int, size of unpaired region
     """
 
-    if not os.path.exists(tmpdir):
-        os.makedirs(tmpdir)
-
     profiles = RNAplfold_PROFILES_EXECUTE
     for i, P in enumerate(profiles):
         print("running {P}_RNAplfold... ({i}/{N})".format(P=P, i=i + 1, N=len(profiles)))
 
         command = "{bin}/{P}_RNAplfold".format(bin=RNAplfold_BIN_DIR, P=P)
-        args = " -W {W} -L {L} -u {U} < {fa} > {tmp}/{P}_profile.fa".format(P=P, W=W, L=L, U=U, fa=input_fasta, tmp=tmpdir)
+        file_out = "{tmp}/{P}_profile.fa".format(tmp=tmpdir, P=P)
+        args = " -W {W} -L {L} -u {U} < {fa} > {file_out}".format(W=W, L=L, U=U, fa=input_fasta, file_out=file_out)
+
         os.system(command + args)
+
+        if os.path.getsize(file_out) == 0:
+            raise Exception("command wrote an empty file: {0}".format(file_out))
+        # TODO - check if the file is empty
     print("done!")
 
 
@@ -59,7 +69,34 @@ def read_RNAplfold(tmpdir, maxlen=None, seq_align="start", pad_with="E"):
     return arr
 
 
-# TODO detailed description of the parameters?
+def list_split(lst, n):
+    return [lst[i::n] for i in range(n)]
+
+
+# not working currently
+# TODO - fix
+def encodeRNAStructure_parallel(seq_vec, maxlen=None, seq_align="start",
+                                W=240, L=160, U=1, n_cores=4,
+                                tmpdir="/tmp/RNAplfold/"):
+
+    if maxlen is None:
+        maxlen = max([len(seq) for seq in seq_vec])
+    n_cores = min(len(seq_vec), n_cores, multiprocessing.cpu_count())
+    seq_vec_chunked = list_split(seq_vec, n_cores)
+
+    p = Pool(n_cores)
+    job_args = [(seq_vec_chunked[i], maxlen, seq_align, W, L, U, "{0}/job_{1}".format(tmpdir, i))
+                for i in range(n_cores)]
+
+    results = p.map(lambda args: encodeRNAStructure(*args), job_args)
+    # results = Parallel(n_jobs=n_cores)(delayed(run_parallel)(i) for i in range(n_cores))
+    # results = [encodeRNAStructure(seq_vec_chunked[i], maxlen, seq_align,
+    #                               W, L, U, n_cores=1,
+    #                               tmpdir="{0}/job_{1}".format(tmpdir, i))
+    #            for i in range(n_cores)]
+    return np.concatenate(results)
+
+
 def encodeRNAStructure(seq_vec, maxlen=None, seq_align="start",
                        W=240, L=160, U=1,
                        tmpdir="/tmp/RNAplfold/"):
@@ -74,7 +111,15 @@ def encodeRNAStructure(seq_vec, maxlen=None, seq_align="start",
     - for fly, yeast   use W, L, u :  80,  40, 1
 
     """
+    if not isinstance(seq_vec, list):
+        seq_vec = seq_vec.tolist()
+    if not os.path.exists(tmpdir):
+        os.makedirs(tmpdir)
+
     fasta_path = tmpdir + "/input.fasta"
     write_fasta(fasta_path, seq_vec)
     run_RNAplfold(fasta_path, tmpdir, W=W, L=L, U=U)
+    # 1. split the fasta into pieces
+    # 2. run_RNAplfold for each of them
+    # 3. Read the results
     return read_RNAplfold(tmpdir, maxlen, seq_align, pad_with="E")
--- tests/preprocessing/test_structure.py
+++ tests/preprocessing/test_structure.py
@@ -1,10 +1,22 @@
-#
+import os
 import numpy as np
 from concise.preprocessing import encodeRNAStructure
-from concise.preprocessing.structure import run_RNAplfold, read_RNAplfold
+from concise.preprocessing.structure import run_RNAplfold, read_RNAplfold, encodeRNAStructure_parallel
+import pandas as pd
+from contextlib import contextmanager
 
 
-def test_data():
+@contextmanager
+def cd(newdir):
+    prevdir = os.getcwd()
+    os.chdir(os.path.expanduser(newdir))
+    try:
+        yield
+    finally:
+        os.chdir(prevdir)
+
+
+def test_basic():
     input_fasta = "tests/preprocessing/RNAplfold_data/input.fasta"
     tmpdir = "/tmp/RNAplfold"
 
@@ -16,10 +28,39 @@ def test_data():
     assert np.allclose(arr.sum(axis=2), 1)
     assert arr.shape == (2, 302, 5)
     # shape = chanells, sequences, values
-    #
-    # what we want: seqs, values, chanells?
-    seq = ["TATTATGTATATGTATA", "TATGTATAT"]
+
+
+def test_encodeRNAstructure():
+    with cd("/tmp/"):
+        # what we want: seqs, values, chanells?
+        seq = ["TATTATGTATATGTATA", "TATGTATAT"]
+
+        arr = encodeRNAStructure(seq)
+    assert np.allclose(arr.sum(axis=2), 1)
+    assert arr.shape == (2, 17, 5)
+
+
+def test_other_objects():
+    seq = np.array(["TATTATGTATATGTATA", "TATGTATAT"])
 
     arr = encodeRNAStructure(seq)
     assert np.allclose(arr.sum(axis=2), 1)
     assert arr.shape == (2, 17, 5)
+
+
+def test_real_data():
+    csv_file_path = "data/pombe_half-life_UTR3.csv"
+    dt = pd.read_csv(csv_file_path)
+    seq_vec = dt["seq"][:6]
+    a = encodeRNAStructure(seq_vec)
+    assert a.shape == (6, seq_vec.str.len().max(), 5)
+
+
+# def test_real_data_parallel():
+#     csv_file_path = "data/pombe_half-life_UTR3.csv"
+#     dt = pd.read_csv(csv_file_path)
+#     seq_vec = dt["seq"][:6]
+#     a = encodeRNAStructure_parallel(seq_vec, n_cores=4)
+#     assert a.shape == (6, seq_vec.str.len().max(), 5)
+
+
