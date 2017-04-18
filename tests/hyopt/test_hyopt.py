19d2e7aa1c59dc41cf6abe34b673245bd2031a96
Avsecz <zigaavsec@gmail.com>
2017-04-18 12:40:51 +0200

    tests: added mongodb unit-tests

--- tests/hyopt/test_hyopt.py
+++ tests/hyopt/test_hyopt.py
@@ -6,9 +6,9 @@ from concise.hyopt import CompileFN, CMongoTrials
 from concise.utils.helper import merge_dicts
 import subprocess
 from tests.hyopt import data, model
+from copy import deepcopy
 
 
-# TODO - lacking more unit-tests for CMongoTrials
 def test_compilefn_train_test_split():
     db_name = "test"
     exp_name = "test2"
@@ -31,6 +31,7 @@ def test_compilefn_train_test_split():
     }
     trials = Trials()
     best = fmin(fn, hyper_params, trials=trials, algo=tpe.suggest, max_evals=2)
+    assert isinstance(best, dict)
 
 
 def test_compilefn_cross_val():
@@ -57,8 +58,7 @@ def test_compilefn_cross_val():
     assert isinstance(best, dict)
 
 
-def manual_test_hyopt(tmpdir):
-
+def test_hyopt(tmpdir):
     # get the base dir
     mongodb_path = str(tmpdir.mkdir('mongodb'))
     results_path = str(tmpdir.mkdir('results'))
@@ -89,8 +89,7 @@ def manual_test_hyopt(tmpdir):
         # stderr=subprocess.PIPE,
         env=merge_dicts(os.environ, {"PYTHONPATH": os.getcwd()}),
     )
-    # wait a bit
-    time.sleep(1)
+    # --------------------------------------------
 
     db_name = "test"
     exp_name = "test2"
@@ -108,14 +107,50 @@ def manual_test_hyopt(tmpdir):
                   },
         "fit": {"epochs": 1}
     }
-    # trials = Trials()
-    # best = fmin(fn, hyper_params, trials=trials, algo=tpe.suggest, max_evals=2)
 
-    trials = CMongoTrials('mongo://localhost:22334/' + db_name + "/jobs", exp_key=exp_name)
+    trials = CMongoTrials(db_name, exp_name, ip="localhost",
+                          kill_timeout=5 * 60,
+                          port=22334)
 
-    best = fmin(fn, hyper_params, trials=trials, algo=tpe.suggest, max_evals=4)
-    mongo_worker_proc.terminate()
-    mongodb_proc.terminate()
-
+    best = fmin(fn, hyper_params, trials=trials, algo=tpe.suggest, max_evals=2)
+    assert len(trials) == 2
     assert isinstance(best, dict)
     assert "m_filters" in best
+
+    # test my custom functions
+    trials.as_df()
+    trials.train_history(trials.valid_tid()[0])
+    trials.train_history(trials.valid_tid())
+    trials.get_ok_results()
+
+    # --------------------------------------------
+    # cross-validation
+    db_name = "test"
+    exp_name = "test2_cv"
+
+    fn = CompileFN(db_name, exp_name,
+                   data_fn=data.data,
+                   model_fn=model.build_model,
+                   cv_n_folds=3,
+                   eval2loss_fn=model.hyperopt_loss_build_model,
+                   save_dir=results_path)
+
+    trials = CMongoTrials(db_name, exp_name, ip="localhost",
+                          kill_timeout=5 * 60,
+                          port=22334)
+
+    best = fmin(fn, deepcopy(hyper_params), trials=trials, algo=tpe.suggest, max_evals=2)
+    assert len(trials) == 2
+    assert isinstance(best, dict)
+    assert "m_filters" in best
+
+    # test my custom functions
+    trials.as_df()
+    trials.train_history(trials.valid_tid()[0])
+    trials.train_history(trials.valid_tid())
+    trials.get_ok_results()
+
+    # --------------------------------------------
+    # close
+    mongo_worker_proc.terminate()
+    mongodb_proc.terminate()
