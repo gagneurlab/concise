5e022af86d299bed55811a3a272e9fd4bf21b3bb
Avsecz <zigaavsec@gmail.com>
2017-04-29 01:33:30 +0200

    feat: implemented all loss function _masked

--- concise/losses.py
+++ concise/losses.py
@@ -6,38 +6,83 @@ from concise.utils.helper import get_from_module
 MASK_VALUE = -1
 
 
-class MaskLoss:
+def mask_loss(loss, mask_value=MASK_VALUE):
+    loss_fn = kloss.deserialize(loss)
 
-    def __init__(self, loss, mask_value=MASK_VALUE):
-        """
-        Compile masked loss function
-        This function ignores values where y_true == mask_value.
-
-        Arguments:
-            loss = loss function from keras.losses
-            mask_value = numeric value to be masked away (np.nan not supported for now)
-
-        Inspired by: https://github.com/fchollet/keras/issues/3893
-        """
-        self.loss = kloss.deserialize(loss)  # TODO - add the ability to create your own loss functions
-        self.mask_value = mask_value
-
-    def __call__(self, y_true, y_pred):
+    def masked_loss_fn(y_true, y_pred):
         # currently not suppoerd with NA's:
         #  - there is no K.is_nan impolementation in keras.backend
         #  - https://github.com/fchollet/keras/issues/1628
-        mask = K.cast(K.not_equal(y_true, self.mask_value), K.floatx())
+        mask = K.cast(K.not_equal(y_true, mask_value), K.floatx())
 
         # we divide by the mean to correct for the number of done loss evaluations
-        return self.loss(y_true * mask, y_pred * mask) / K.mean(mask)
+        return loss_fn(y_true * mask, y_pred * mask) / K.mean(mask)
 
-    def get_config(self):
-        return {"loss": kloss.serialize(self.loss),
-                "mask_value": self.mask_value
-                }
+    masked_loss_fn.__name__ = loss + "_masked"
+    return masked_loss_fn
 
 
-AVAILABLE = ["MaskLoss"]
+# Bellow would be the most general case and wouldn't reqire hard-coding the values
+# However, it doesn't work with the classes as the current serialization is with .__name__
+# and not with serialize_keras_object
+# https://github.com/fchollet/keras/blob/master/keras/metrics.py#L48
+#
+# class MaskLoss:
+
+#     __name__ = "MaskLoss"
+
+#     def __init__(self, loss, mask_value=MASK_VALUE):
+#         """
+#         Compile masked loss function
+#         This function ignores values where y_true == mask_value.
+
+#         Arguments:
+#             loss = loss function from keras.losses
+#             mask_value = numeric value to be masked away (np.nan not supported for now)
+
+#         Inspired by: https://github.com/fchollet/keras/issues/3893
+#         """
+#         self.loss = kloss.deserialize(loss)  # TODO - add the ability to create your own loss functions
+#         self.mask_value = mask_value
+
+#     def __call__(self, y_true, y_pred):
+#         # currently not suppoerd with NA's:
+#         #  - there is no K.is_nan impolementation in keras.backend
+#         #  - https://github.com/fchollet/keras/issues/1628
+#         mask = K.cast(K.not_equal(y_true, self.mask_value), K.floatx())
+
+#         # we divide by the mean to correct for the number of done loss evaluations
+#         return self.loss(y_true * mask, y_pred * mask) / K.mean(mask)
+
+#     def get_config(self):
+#         return {"loss": kloss.serialize(self.loss),
+#                 "mask_value": self.mask_value
+#                 }
+
+
+# masked loss functions
+AVAILABLE = ["mean_squared_error_masked",
+             "mean_absolute_error_masked",
+             "mean_absolute_percentage_error_masked",
+             "mean_squared_logarithmic_error_masked",
+             "squared_hinge_masked",
+             "hinge_masked",
+             "categorical_crossentropy_masked",
+             "sparse_categorical_crossentropy_masked",
+             "binary_crossentropy_masked",
+             "kullback_leibler_divergence_masked"]
+
+# NOTE - name has to be <loss>_mask
+mean_squared_error_masked = mask_loss("mean_squared_error")
+mean_absolute_error_masked = mask_loss("mean_absolute_error")
+mean_absolute_percentage_error_masked = mask_loss("mean_absolute_percentage_error")
+mean_squared_logarithmic_error_masked = mask_loss("mean_squared_logarithmic_error")
+squared_hinge_masked = mask_loss("squared_hinge")
+hinge_masked = mask_loss("hinge")
+categorical_crossentropy_masked = mask_loss("categorical_crossentropy")
+sparse_categorical_crossentropy_masked = mask_loss("sparse_categorical_crossentropy")
+binary_crossentropy_masked = mask_loss("binary_crossentropy")
+kullback_leibler_divergence_masked = mask_loss("kullback_leibler_divergence")
 
 
 def get(name):
--- tests/test_losses.py
+++ tests/test_losses.py
@@ -1,11 +1,14 @@
 import keras.losses as kloss
-from concise.losses import MaskLoss, MASK_VALUE
+import concise.losses as closs
+from concise.losses import MASK_VALUE
 import numpy as np
 import keras.backend as K
+import keras.layers as kl
+from keras.models import Model, load_model
 from keras.utils.generic_utils import deserialize_keras_object, serialize_keras_object, get_custom_objects
 
 def test_MaskLoss():
-    l = MaskLoss("binary_crossentropy")
+    l = closs.binary_crossentropy_masked
     y_pred = np.array([0, 0.2, 0.6, 0.4, 1])
     y_true = np.array([1, 0, -1, 1, 0.0])
 
@@ -22,8 +25,28 @@ def test_MaskLoss():
 
     # test serialization
     s = serialize_keras_object(l)
-    a = deserialize_keras_object(s, custom_objects={"MaskLoss": MaskLoss})
-    assert a.loss == l.loss
-    assert a.mask_value == l.mask_value
+    a = deserialize_keras_object(s)
+    # assert a.loss == l.loss
+    # assert a.mask_value == l.mask_value
     res2 = K.eval(a(y_true, y_pred))
     assert np.allclose(res, res2)
+
+
+def test_ConvDNAQuantitySplines(tmpdir):
+
+    x = np.vstack([np.arange(15), np.arange(15)])
+    y = np.arange(2)
+
+    inl = kl.Input((15,))
+
+    o = kl.Dense(1)(inl)
+    model = Model(inl, o)
+    model.compile("Adam", loss=closs.binary_crossentropy_masked)
+    model.fit(x, y)
+
+    filepath = str(tmpdir.mkdir('data').join('test_keras.h5'))
+
+    # load and save the model
+    model.save(filepath)
+    m = load_model(filepath)
+    assert isinstance(m, Model)
