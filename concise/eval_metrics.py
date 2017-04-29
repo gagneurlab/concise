a4f2782c1bced9c90a76c7b0ded4088b309c74a4
Avsecz <zigaavsec@gmail.com>
2017-04-29 23:34:36 +0200

    feat: remove np.nan values in eval_metrics

--- concise/eval_metrics.py
+++ concise/eval_metrics.py
@@ -8,15 +8,36 @@ import numpy as np
 from concise.utils.helper import get_from_module
 import sklearn.metrics as skm
 from scipy.stats import kendalltau
+from concise.metrics import MASK_VALUE
 # TODO - make them equivalent to metrics
 # TODO - test them
-# TODO - implement NA values
 
 
 # Binary classification
 # ----
 
+def _mask_nan(y, z):
+    mask_array = ~np.isnan(y)
+    if np.any(np.isnan(z)):
+        print("WARNING: y_pred contains {0}/{1} np.nan values. removing them...".
+              format(np.sum(np.isnan(z)), z.size))
+        mask_array = np.logical_and(mask_array, ~np.isnan(z))
+    return y[mask_array], z[mask_array]
+
+
+def _mask_value(y, z, mask=MASK_VALUE):
+    mask_array = y != mask
+    return y[mask_array], z[mask_array]
+
+
+def _mask_value_nan(y, z, mask=MASK_VALUE):
+    y, z = _mask_nan(y, z)
+    return _mask_value(y, z, mask)
+
+
 def auc(y, z, round=True):
+    y, z = _mask_value_nan(y, z)
+
     if round:
         y = y.round()
     if len(y) == 0 or len(np.unique(y)) < 2:
@@ -25,10 +46,12 @@ def auc(y, z, round=True):
 
 
 def auprc(y, z):
+    y, z = _mask_value_nan(y, z)
     return skm.average_precision_score(y, z)
 
 
 def acc(y, z, round=True):
+    y, z = _mask_value_nan(y, z)
     if round:
         y = np.round(y)
         z = np.round(z)
@@ -36,6 +59,7 @@ def acc(y, z, round=True):
 
 
 def tpr(y, z, round=True):
+    y, z = _mask_value_nan(y, z)
     if round:
         y = np.round(y)
         z = np.round(z)
@@ -43,6 +67,7 @@ def tpr(y, z, round=True):
 
 
 def tnr(y, z, round=True):
+    y, z = _mask_value_nan(y, z)
     if round:
         y = np.round(y)
         z = np.round(z)
@@ -51,6 +76,7 @@ def tnr(y, z, round=True):
 
 
 def mcc(y, z, round=True):
+    y, z = _mask_value_nan(y, z)
     if round:
         y = np.round(y)
         z = np.round(z)
@@ -58,6 +84,7 @@ def mcc(y, z, round=True):
 
 
 def f1(y, z, round=True):
+    y, z = _mask_value_nan(y, z)
     if round:
         y = np.round(y)
         z = np.round(z)
@@ -76,10 +103,12 @@ def cat_acc(y, z):
 
 def cor(y, z):
     """Compute Pearson correlation coefficient."""
+    y, z = _mask_nan(y, z)
     return np.corrcoef(y, z)[0, 1]
 
 
 def kendall(y, z, nb_sample=100000):
+    y, z = _mask_nan(y, z)
     if len(y) > nb_sample:
         idx = np.arange(len(y))
         np.random.shuffle(idx)
@@ -90,6 +119,7 @@ def kendall(y, z, nb_sample=100000):
 
 
 def mad(y, z):
+    y, z = _mask_nan(y, z)
     return np.mean(np.abs(y - z))
 
 
@@ -102,9 +132,8 @@ def rrmse(y, z):
 
 
 def mse(y_true, y_pred):
-    # equivalent to TF representation
-    y_diff = np.where(np.isnan(y_pred) | np.isnan(y_true), 0, y_true - y_pred)
-    return ((y_diff) ** 2).mean(axis=None)
+    y_true, y_pred = _mask_nan(y_true, y_pred)
+    return ((y_true - y_pred) ** 2).mean(axis=None)
 
 
 def ermse(y_true, y_pred):
@@ -116,6 +145,7 @@ def ermse(y_true, y_pred):
 def var_explained(y_true, y_pred):
     """Fraction of variance explained.
     """
+    y_true, y_pred = _mask_nan(y_true, y_pred)
     var_resid = np.var(y_true - y_pred)
     var_y_true = np.var(y_true)
     return 1 - var_resid / var_y_true
