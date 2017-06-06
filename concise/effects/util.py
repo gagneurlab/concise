7ceefd936181945891f80b0359f81eb8bc812994
Roman Kreuzhuber <rkreuzhu@hh-yoda-04-01.ebi.ac.uk>
2017-06-06 15:24:10 +0100

    Fixed gradient.py, sim.py and dropout.py to work with theano as well as with models with list type input. Also added a list type model for testing.

---
+++ concise/effects/util.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def get_seq_len(input_data):
+    if isinstance(input_data, (list, tuple)):
+        return input_data[0].shape
+    elif isinstance(input_data, dict):
+        for k in input_data:
+            return input_data[k].shape
+    elif isinstance(input_data, np.ndarray):
+        return input_data.shape
+    else:
+        raise ValueError("Input can only be of type: list, dict or np.ndarray")
+
